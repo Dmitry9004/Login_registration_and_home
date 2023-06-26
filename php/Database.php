fba73013a26cef6e90f5026a453437164cc4606c
Dmitry9004 <117633827+Dmitry9004@users.noreply.github.com>
2023-06-26 14:11:58 +0300

    Update Database.php

--- php/Database.php
+++ php/Database.php
@@ -1,7 +1,23 @@
 <?php
+//!!!!!!! SQL FOR CREATE DATABASE AND TABLES IN MYSQL
+//
+//CREATE DATABASE users;
+//
+//CREATE TABLE users(
+// id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+// login varchar(255) NOT NULL UNIQUE,
+// password varchar(255) NOT NULL,
+// name varchar(255) NOT NULL,
+// photo varchar(255) NOT NULL,
+// date_birth date NOT NULL
+//)
+//
+
+
 	$conn = null;
 	 function get_connection(){
 	 		try{
+				// here your servername, username, password, nameDB
 				$conn = mysqli_connect("127.0.0.1", "root", "", "users");
 			}catch(Exception $e){
 				return null;		
@@ -15,6 +31,7 @@
 	$conn_enter = null;
 	function get_connection_enter(){
 		try{
+			// here your servername, username, password, nameDB
 			$conn_enter = mysqli_connect("127.0.0.1", "root", "", "users_enter");
 		}catch(Exception $e){
 			return null;
